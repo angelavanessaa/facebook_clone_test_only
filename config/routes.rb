879fe5cbe8ce21487d77ffec957f831d83daf926
Angela <Vanessa>
2016-08-18 18:00:21 +0700

    okay now done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 	resources :likes, only: [:create, :destroy]
 	resources :users, only: [:new, :create, :edit, :update, :destroy]
 	resources :sessions, only: [:new, :create]
-	delete '/logout', to: "sessions#destroy", as: "logout"
+	delete '/logout', to: "sessions#destroy", as: "signout"
   resources :statuses, only: [:new, :create, :edit, :update, :destroy]
 end
